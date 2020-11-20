e61d0fd25d383ce4c103ebe4029ca9b13ef0708f
achuljyan <achuljyan@questrade.com>
2020-11-20 13:32:46 +0000

    names = > name

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -17,7 +17,7 @@ describe('API Pact test', () => {
   describe('retrieving a product', () => {
     test('ID 10 exists', async () => {
       // Arrange
-      const expectedProduct = { id: '10', type: 'CREDIT_CARD', names: '28 Degrees' }
+      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees' }
 
       await mockProvider.addInteraction({
         state: 'a product with ID 10 exists',
@@ -34,7 +34,8 @@ describe('API Pact test', () => {
           headers: {
             'Content-Type': regex({generate: 'application/json; charset=utf-8', matcher: 'application/json;?.*'}),
           },
-          body: like(expectedProduct),
+//          body: like(expectedProduct),
+            body: like(expectedProduct),
         },
       });
 
@@ -73,7 +74,7 @@ describe('API Pact test', () => {
   describe('retrieving products', () => {
     test('products exists', async () => {
       // set up Pact interactions
-      const expectedProduct = { id: '10', type: 'CREDIT_CARD', names: '28 Degrees' }
+      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees' }
 
       await mockProvider.addInteraction({
         state: 'products exist',
