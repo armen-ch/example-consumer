a8345a70132ec2a9830c8b0ee6f6a941ccee1df5
achuljyan <achuljyan@questrade.com>
2020-12-16 21:40:51 +0000

    201 to 200

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -87,7 +87,7 @@ describe('API Pact test', () => {
           },
         },
         willRespondWith: {
-          status: 201,
+          status: 200,
           headers: {
             'Content-Type': regex({generate: 'application/json; charset=utf-8', matcher: 'application/json;?.*'}),
           },
